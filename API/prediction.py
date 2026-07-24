2a565c064c740b8b8205e9116ca3d45b42e27d92
teniolaiji <teniolaiji@gmail.com>
2026-07-24 18:34:55 +0200

    Save plots

--- API/prediction.py
+++ API/prediction.py
@@ -216,33 +216,3 @@ async def retrain(file: UploadFile = File(...)):
         "new_rmse": round(new_rmse, 3)
     }
 
-# def predict_poverty(continuous_inputs: dict, region: str):
-#     """
-#     continuous_inputs: {feature_name: value} for the non-region features.
-#     region: one region string from REGION_MAP.
-#     Returns predicted poverty ratio (% below $3.00/day).
-#     """
-#     row = dict(continuous_inputs)
-
-#     for col in REGION_COLUMNS:
-#         row[col] = 0
-#     active = REGION_MAP.get(region)
-#     if active is not None:
-#         row[active] = 1
-
-#     ordered = [row[name] for name in feature_names]
-#     X_new = np.array(ordered, dtype=float).reshape(1, -1)
-#     X_scaled = scaler.transform(X_new)
-
-#     return round(float(model.predict(X_scaled)[0]), 2)
-
-
-# if __name__ == '__main__':
-#     example = {
-#         'electricity_access': 45.0, 'agri_value_added': 25.0,
-#         'health_expenditure': 5.0, 'gdp_growth': 3.0,
-#         'gdp_per_capita': 800.0, 'education_expenditure': 4.0,
-#         'internet_users': 20.0, 'inflation': 8.0,
-#         'life_expectancy': 60.0, 'urban_population': 35.0,
-#     }
-#     print(predict_poverty(example, 'Sub-Saharan Africa'))
